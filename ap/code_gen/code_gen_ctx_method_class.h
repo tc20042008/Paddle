0b33a0b365e0cd3ee3cb514ff4e9daa04659dcc4
jiahy0825 <jiahongyu@baidu.com>
2024-11-12 07:47:44 +0000

    refactor code_gen_ctx.cuda_code_gen to code_gen_ctx.op_code_gen

--- ap/code_gen/code_gen_ctx_method_class.h
+++ ap/code_gen/code_gen_ctx_method_class.h
@@ -45,8 +45,8 @@ struct CodeGenCtxMethodClass {
 
   adt::Result<ValueT> GetAttr(const Self& self, const ValueT& attr_name_val) {
     ADT_LET_CONST_REF(attr_name, axpr::TryGetImpl<std::string>(attr_name_val));
-    if (attr_name == "cuda_code_gen") {
-      return axpr::Method<ValueT>{self, &This::StaticCudaCodeGen};
+    if (attr_name == "op_code_gen") {
+      return axpr::Method<ValueT>{self, &This::StaticOpCodeGen};
     }
     if (attr_name == "render_module_template") {
       return axpr::Method<ValueT>{self, &This::StaticRenderModuleTemplate};
@@ -259,10 +259,10 @@ struct CodeGenCtxMethodClass {
     return getter;
   }
 
-  static adt::Result<ValueT> StaticCudaCodeGen(
-      const ValueT& self_val, const std::vector<ValueT>& args) {
+  static adt::Result<ValueT> StaticOpCodeGen(const ValueT& self_val,
+                                             const std::vector<ValueT>& args) {
     ADT_LET_CONST_REF(self, axpr::TryGetImpl<Self>(self_val));
-    return This{}.CudaCodeGen(self, args);
+    return This{}.OpCodeGen(self, args);
   }
 
   static adt::Result<ValueT> StaticRenderModuleTemplate(
@@ -275,24 +275,24 @@ struct CodeGenCtxMethodClass {
 
   using NativeOrRefIrValue = ir_match::NativeOrRefIrValue<BirNode>;
 
-  adt::Result<ValueT> CudaCodeGen(const Self& self,
-                                  const std::vector<ValueT>& packed_args_vec) {
+  adt::Result<ValueT> OpCodeGen(const Self& self,
+                                const std::vector<ValueT>& packed_args_vec) {
     const auto& packed_args = axpr::CastToPackedArgs(packed_args_vec);
     const auto& [args, kwargs] = *packed_args;
     ADT_CHECK(args->size() == 1) << adt::errors::TypeError{
-        "'CodeGenCtx.cuda_code_gen' takes 1 positional arguments but " +
+        "'CodeGenCtx.op_code_gen' takes 1 positional arguments but " +
         std::to_string(args->size()) + " were given."};
     ADT_LET_CONST_REF(ir_op, IrOp<BirNode>::CastFrom(args->at(0)))
         << adt::errors::TypeError{
                std::string() +
-               "the positional argument 1 of 'CodeGenCtx.cuda_code_gen' should "
+               "the positional argument 1 of 'CodeGenCtx.op_code_gen' should "
                "be able to cast to a NativeIrOp, PackedIrOp or RefIrOp."};
     ADT_LET_CONST_REF(kernel_arg_id2arg_name_val,
                       kwargs->template Get<axpr::OrderedDict<ValueT>>(
                           "kernel_arg_id_to_arg_name"))
         << adt::errors::TypeError{
                std::string() +
-               "cuda_code_gen() requires 'OrderedDict' typed keyword argument "
+               "op_code_gen() requires 'OrderedDict' typed keyword argument "
                "'kernel_arg_id_to_arg_name'."};
     ADT_LET_CONST_REF(kernel_arg_id2arg_name,
                       CastToKernelArgId2Name(kernel_arg_id2arg_name_val));
@@ -301,24 +301,23 @@ struct CodeGenCtxMethodClass {
                           "loop_index_tuple_expr"))
         << adt::errors::TypeError{
                std::string() +
-               "'CodeGenCtx.cuda_code_gen' requires 'IndexTupleExpr' typed "
+               "'CodeGenCtx.op_code_gen' requires 'IndexTupleExpr' typed "
                "keyword argument 'loop_index_tuple_expr'."};
     std::vector<std::string> loop_var_names{};
     {
       ADT_LET_CONST_REF(
           loop_var_names_val,
           kwargs->template Get<adt::List<ValueT>>("loop_var_names"))
-          << adt::errors::TypeError{
-                 std::string() +
-                 "'CodeGenCtx.cuda_code_gen' requires 'list' "
-                 "typed keyword argument 'loop_var_names'."};
+          << adt::errors::TypeError{std::string() +
+                                    "'CodeGenCtx.op_code_gen' requires 'list' "
+                                    "typed keyword argument 'loop_var_names'."};
       loop_var_names.reserve(loop_var_names_val->size());
       for (const auto& elt : *loop_var_names_val) {
         ADT_LET_CONST_REF(loop_var_name, axpr::TryGetImpl<std::string>(elt))
             << adt::errors::TypeError{
                    std::string() +
                    "keyword argument 'loop_var_names' of "
-                   "'CodeGenCtx.cuda_code_gen' should be a list of string."};
+                   "'CodeGenCtx.op_code_gen' should be a list of string."};
         loop_var_names.emplace_back(loop_var_name);
       }
     }
@@ -327,40 +326,31 @@ struct CodeGenCtxMethodClass {
         kwargs->template GetOpt<std::string>("anchor_local_var_name"))
         << adt::errors::TypeError{std::string() +
                                   "keyword argument 'anchor_local_var_name' of "
-                                  "'CodeGenCtx.cuda_code_gen' should be a str"};
+                                  "'CodeGenCtx.op_code_gen' should be a str"};
     std::vector<LocalVarBinding<BirNode>> local_var_name_bindings{};
     {
-      ADT_LET_CONST_REF(
-          local_var_name_bindings_val,
-          kwargs->template Get<adt::List<ValueT>>("local_var_name_bindings"))
+      ADT_LET_CONST_REF(ir_value_to_local_var_name,
+                        kwargs->template Get<axpr::OrderedDict<ValueT>>(
+                            "ir_value_to_local_var_name"))
           << adt::errors::TypeError{
                  std::string() +
-                 "'CodeGenCtx.cuda_code_gen' requires 'list' typed keyword "
+                 "'CodeGenCtx.op_code_gen' requires 'list' typed keyword "
                  "argument 'local_var_name_bindings'."};
-      local_var_name_bindings.reserve(local_var_name_bindings_val->size());
-      for (const auto& elt : *local_var_name_bindings_val) {
-        ADT_LET_CONST_REF(pair, axpr::TryGetImpl<adt::List<ValueT>>(elt))
+      local_var_name_bindings.reserve(
+          ir_value_to_local_var_name->items().size());
+      for (const auto& [k, v] : ir_value_to_local_var_name->items()) {
+        ADT_LET_CONST_REF(local_var_name, axpr::TryGetImpl<std::string>(v))
             << adt::errors::TypeError{
                    std::string() +
-                   "keyword argument 'local_var_name_bindings' of "
-                   "'CodeGenCtx.cuda_code_gen' should be a list of pair."};
-        ADT_CHECK(pair->size() == 2) << adt::errors::TypeError{
-            std::string() +
-            "keyword argument 'local_var_name_bindings' of "
-            "'CodeGenCtx.cuda_code_gen' should be a list of pair."};
-        ADT_LET_CONST_REF(local_var_name,
-                          axpr::TryGetImpl<std::string>(pair->at(0)))
+                   "keyword argument 'ir_value_to_local_var_name' of "
+                   "'CodeGenCtx.op_code_gen' should be OrderedDict[str, "
+                   "'NativeIrValue|RefIrValue']."};
+        ADT_LET_CONST_REF(ir_tensor, NativeOrRefIrValue::CastFrom(k))
             << adt::errors::TypeError{
                    std::string() +
-                   "keyword argument 'local_var_name_bindings' of "
-                   "'CodeGenCtx.cuda_code_gen' should be a list of pair(str, "
-                   "NativeIrValue)."};
-        ADT_LET_CONST_REF(ir_tensor, NativeOrRefIrValue::CastFrom(pair->at(1)))
-            << adt::errors::TypeError{
-                   std::string() +
-                   "keyword argument 'local_var_name_bindings' of "
-                   "'CodeGenCtx.cuda_code_gen' should be a list of pair(str, "
-                   "NativeIrValue/RefIrValue)."};
+                   "keyword argument 'ir_value_to_local_var_name' of "
+                   "'CodeGenCtx.op_code_gen' should be OrderedDict[str, "
+                   "'NativeIrValue|RefIrValue']."};
         LocalVarBinding<BirNode> binding{local_var_name, ir_tensor};
         local_var_name_bindings.emplace_back(binding);
       }
